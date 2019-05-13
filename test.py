1cd8a090680c4957f88f4805cb95621a94f65e9d
Verina Cristie <cristie_verina@sutd.edu.sg>
2019-05-14 00:26:41 +0800

    function to add two numbers and print result

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
+def add(a,b):
+    result = float(a) + float(b)
+    print("add: ",result)
+
 num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
 print("num1: ", num1, " num2: ", num2)
+add(num1,num2)
